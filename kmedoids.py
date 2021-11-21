c498df9442eda0ec4690fd86155288ce8b1592b4
Nam Vu <van.nam.vu@umontreal.ca>
2021-11-21 13:58:26 -0500

    update main.py and kmedoids

--- kmedoids.py
+++ kmedoids.py
@@ -7,21 +7,21 @@ from numpy.random import choice
 from numpy.random import seed
 import matplotlib.pyplot as plt
 #import mnist_similarity
-from mnist_similarity import mnist_dissimilarity
-from mnist_similarity import mnist_import_Xtrain_Ytrain_matDiss
+#from mnist_similarity import mnist_dissimilarity
+#from mnist_similarity import mnist_import_Xtrain_Ytrain_matDiss
 #import preprocessing_mnist
 #from preprocessing_mnist import import_data
 
 # X = []  # data to be imported
 # D = []  # table of dissimilarity
-print('###------ Algorithme K-medoids ------####')
-# D = mat_dissimilarity()  # recuperer la matrice de dissimilarite de tous les points
-print("---importing data.....")
-x_train, y_train, x_test, y_test, D_train, D_test = mnist_import_Xtrain_Ytrain_matDiss()
-print('# Data train = ' + str(len(x_train)))
-print('# Data test = ' + str(len(x_test)))
-print(np.shape(D_train))
-print("- imported ")
+
+# print('###------ Algorithme K-medoids ------####')
+#print("---importing data.....")
+#x_train, y_train, x_test, y_test, D_train, D_test = mnist_import_Xtrain_Ytrain_matDiss()
+# print('# Data train = ' + str(len(x_train)))
+# print('# Data test = ' + str(len(x_test)))
+# print(np.shape(D_train))
+#print("- imported ")
 
 
 def init_medoids(X, k):
@@ -35,33 +35,7 @@ def init_medoids(X, k):
     return set_medoids
 
 
-print("Step execute K-medoids")
-initial_medoids = init_medoids(x_train, 10)
-# for i in range(len(initial_medoids)):
-#    print(y_train[initial_medoids[i]])
-
-initial_medoids1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-# ici, nous donnons directement notre matrice de distance
-# il faut spécifier data_type='distance_matrix'
-kmedoids_instance = kmedoids(
-    D_train, initial_medoids, data_type='distance_matrix')
-print("-- processing....")
-kmedoids_instance.process()
-
-print(" - done")
-clusters = kmedoids_instance.get_clusters()  # groupe pour chaque elements
-medoids = kmedoids_instance.get_medoids()
-
-print('Cluster form ' + str(np.shape(clusters)))
-#print("Cluster : " + str(clusters))
-print("Medoids : " + str(medoids))
-#print('Medoides: -- Classe -- vrai valeur ')
-# for i in medoids:
-#    print(str(i) + "  -- +  +  ------ " +
-#          str(y_train[i]))  # + str(clusters[i]))
-
-
-def associate_clustering(clusters):
+def associate_clustering(y_train, clusters):
     associate_clustering = []
 
     for group in clusters:
@@ -76,32 +50,63 @@ def associate_clustering(clusters):
     return associate_clustering
 
 
-class_medoids = associate_clustering(clusters)
-print('Class medoids : ' + str(class_medoids))
-
-
-def accuracy(x_train, x_test, y_test, medoids, class_medoids):
+def y_prediction(D_test, medoids, class_medoids):
 
     y_predict = []
-    y_trueValue = []
-    for i in range(len(x_test)):
+    #y_trueValue = []
+    for i in range(len(D_test)):
         array = []
         for j in range(len(medoids)):
             # calcule dissimilarite avec les centroids
-            array.append(mnist_dissimilarity(x_test[i], x_train[medoids[j]]))
+            #array.append(mnist_dissimilarity(x_test[i], x_train[medoids[j]]))
+            array.append(D_test[i][medoids[j]])
 
         IndexminValue = np.argmin(array)  # retrieve index for class medoids
         y_predict.append(class_medoids[IndexminValue])
-        y_trueValue.append(y_test[i])
+        # y_trueValue.append(y_test[i])
 
-    a = np.array(y_predict)
-    b = np.array(y_trueValue)
+    return y_predict
+
+    #a = np.array(y_predict)
+    #b = np.array(y_trueValue)
     # print(y_predict)
     # print(y_test)
-    succes = np.sum(a == b)
-    print(succes)
-    return round(succes/len(x_test), 5)
+    #succes = np.sum(a == b)
+    # print(succes)
+    # return round(succes/len(x_test), 5)
 
 
-print('Accuracy = ' + str(accuracy(x_train, x_test, y_test,
-                                   medoids, class_medoids)))
+def kmedoids_processing(x_train, y_train, D_train, D_test, k):
+    y_predict = []
+    #print("Step execute K-medoids")
+    initial_medoids = init_medoids(x_train, k)
+# for i in range(len(initial_medoids)):
+#    print(y_train[initial_medoids[i]])
+
+#initial_medoids1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# ici, nous donnons directement notre matrice de distance
+# il faut spécifier data_type='distance_matrix'
+    kmedoids_instance = kmedoids(
+        D_train, initial_medoids, data_type='distance_matrix')
+#print("-- processing....")
+    kmedoids_instance.process()
+
+#print(" - done")
+    clusters = kmedoids_instance.get_clusters()  # groupe pour chaque elements
+    medoids = kmedoids_instance.get_medoids()
+    class_medoids = associate_clustering(y_train, clusters)
+    print('Class medoids : ' + str(class_medoids))
+
+    y_predict = y_prediction(D_test, medoids, class_medoids)
+    return y_predict
+
+#print('Cluster form ' + str(np.shape(clusters)))
+#print("Cluster : " + str(clusters))
+#print("Medoids : " + str(medoids))
+#print('Medoides: -- Classe -- vrai valeur ')
+# for i in medoids:
+#    print(str(i) + "  -- +  +  ------ " +
+#          str(y_train[i]))  # + str(clusters[i]))
+
+    # print('Accuracy = ' + str(accuracy(x_train, x_test, y_test,
+    #                               medoids, class_medoids)))
--- main.py
+++ main.py
@@ -1,1 +1,74 @@
-# fichier main!
+# Xuan - Matricule
+# Van Nam Vu - Matricule : 20170148
+# -------------------------------------------
+# Travailler sur 2 datasets : MNIST et Adults
+# Fichier main pour generer les algorithmes
+# et afficher les resultats pour evaluer la performance
+
+from pyclustering.cluster.kmedoids import kmedoids
+from numpy.random import choice
+from numpy.random import seed
+
+from sklearn.cluster import AgglomerativeClustering
+import numpy as np
+from mnist_similarity import mnist_import_data
+#import scipy.cluster._hierarchy as shc
+import matplotlib.pyplot as plt
+
+from sklearn.neighbors import KNeighborsClassifier  # k-plus proches voisins
+
+from sklearn.manifold import Isomap
+from sklearn import neighbors
+
+from knn import knn_processing
+from kmedoids import kmedoids_processing
+# fonction pour calculer l'exactitude de l'algorithme
+
+
+def accuracy(y_predict, y_trueValue):
+    a = np.array(y_predict)
+    b = np.array(y_trueValue)
+    # print(y_predict)
+    # print(y_test)
+    succes = np.sum(a == b)
+    # print(succes)
+    return round(succes/len(y_predict), 5)
+
+
+print('###------ Dataset MNIST ------####')
+print("---importing data.....")
+mnistX_train, mnistY_train, mnistX_test, mnistY_test, mnistD_train, mnistD_test = mnist_import_data()
+print('# Data train = ' + str(len(mnistX_train)))
+print('# Data test = ' + str(len(mnistX_test)))
+print(np.shape(mnistD_train))
+print(np.shape(mnistD_test))
+print("- imported ")
+print('\n')
+
+print('---------- 1) Algorithme KNN ----------')
+print('... Processing ....')
+kneighbors_mnist = 1
+print('Execute avec ' + str(kneighbors_mnist)+'-NN')
+knn_mnist = knn_processing(mnistD_train, mnistD_test,
+                           mnistY_train, kneighbors_mnist)
+print(' -> done processing')
+print('-- Etape evaluation : ')
+# print(knn_mnist)
+score = accuracy(knn_mnist, mnistY_test)
+print('Accuracy = ' + str(score))
+print('----')
+
+print('\n')
+print('---------- 2) Algorithme K-medoids ----------')
+print('... Processing ....')
+kmedoids_mnist = 10
+print('Execute avec ' + str(kmedoids_mnist)+'-medoids')
+kmedoids_mnist_predict = kmedoids_processing(
+    mnistD_train, mnistY_train, mnistD_train, mnistD_test, kmedoids_mnist)
+
+print(' -> done processing')
+print('-- Etape evaluation : ')
+# print(knn_mnist)
+score = accuracy(kmedoids_mnist_predict, mnistY_test)
+print('Accuracy = ' + str(score))
+print('----')
