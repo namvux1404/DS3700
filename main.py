ba6eb68283b81a139c696edcea222aaaf7916aff
Nam Vu <van.nam.vu@umontreal.ca>
2021-11-21 15:26:50 -0500

    Update partie MNIST

--- main.py
+++ main.py
@@ -5,27 +5,17 @@
 # Fichier main pour generer les algorithmes
 # et afficher les resultats pour evaluer la performance
 
-#from pyclustering.cluster.kmedoids import kmedoids
-#from numpy.random import choice
-#from numpy.random import seed
-
-#from sklearn.cluster import AgglomerativeClustering
-#
-
-#import scipy.cluster._hierarchy as shc
-#import matplotlib.pyplot as plt
-
-# from sklearn.neighbors import KNeighborsClassifier  # k-plus proches voisins
-
-#from sklearn.manifold import Isomap
-#from sklearn import neighbors
 import numpy as np
 from mnist_similarity import mnist_import_data
+
 from knn import knn_processing
 from kmedoids import kmedoids_processing
+from isomap import isomap_processing
+from pcoa import pcoa_processing
+from partitionBinaire import PB_processing
+
+
 # fonction pour calculer l'exactitude de l'algorithme
-
-
 def accuracy_mnist(y_predict, y_trueValue):
     a = np.array(y_predict)
     b = np.array(y_trueValue)
@@ -55,21 +45,73 @@ knn_mnist = knn_processing(mnistD_train, mnistD_test,
 print(' -> done processing')
 print('-- Etape evaluation : ')
 # print(knn_mnist)
-score = accuracy_mnist(knn_mnist, mnistY_test)
-print('Accuracy = ' + str(score))
+score_knn = accuracy_mnist(knn_mnist, mnistY_test)
+print('Accuracy = ' + str(score_knn))
 print('----')
-
 print('\n')
+
 print('---------- 2) Algorithme K-medoids ----------')
 print('... Processing ....')
 kmedoids_mnist = 10
 print('Execute avec ' + str(kmedoids_mnist)+'-medoids')
 kmedoids_mnist_predict = kmedoids_processing(
-    mnistD_train, mnistY_train, mnistD_train, mnistD_test, kmedoids_mnist)
+    mnistX_train, mnistY_train, mnistD_train, mnistD_test, kmedoids_mnist)
 
 print(' -> done processing')
 print('-- Etape evaluation : ')
 # print(knn_mnist)
-score = accuracy_mnist(kmedoids_mnist_predict, mnistY_test)
-print('Accuracy = ' + str(score))
+score_kmedoids = accuracy_mnist(kmedoids_mnist_predict, mnistY_test)
+print('Accuracy = ' + str(score_kmedoids))
 print('----')
+print('\n')
+
+print('---------- 3) Algorithme Isomap + application KNN ----------')
+print('... Processing isomap ....')
+n_comp_isomap = 8
+print('Reduit dimentionnel à ' + str(n_comp_isomap))
+#knn_isomap = 11
+#print('Execute avec ' + str(knn_isomap)+'-NN')
+isomap_mnist = isomap_processing(
+    mnistD_train, mnistD_test, mnistY_train, mnistY_test, n_comp_isomap)
+
+#print(' -> done processing')
+#print('-- Etape evaluation : ')
+# print(knn_mnist)
+#score_isomap = accuracy_mnist(isomap_mnist, mnistY_test)
+#print('Accuracy = ' + str(score_isomap))
+print('----')
+print('\n')
+
+print('---------- 4) Algorithme PCoA + application KNN ----------')
+print('... Processing isomap ....')
+
+n_comp_pcoa = 8
+print('Reduit dimentionnel à ' + str(n_comp_pcoa))
+#knn_pcoa = 8
+#print('Execute avec ' + str(knn_pcoa)+'-NN')
+pcoa_mnist = pcoa_processing(
+    mnistD_train, mnistD_test, mnistY_train, mnistY_test, n_comp_pcoa)
+
+#print(' -> done processing')
+#print('-- Etape evaluation : ')
+# print(knn_mnist)
+#score_pcoa = accuracy_mnist(pcoa_mnist, mnistY_test)
+#print('Accuracy = ' + str(score_pcoa))
+print('----')
+print('\n')
+
+print('---------- 5) Algorithme Partition Binaire ----------')
+print('... Processing ....')
+cluster_mnist = 30
+print('Execute avec ' + str(cluster_mnist)+' partitions')
+pb_mnist_predict = PB_processing(
+    mnistD_train, mnistD_test, mnistY_train, cluster_mnist)
+
+print(' -> done processing')
+print('-- Etape evaluation : ')
+# print(knn_mnist)
+score_partitionBinaire = accuracy_mnist(pb_mnist_predict, mnistY_test)
+print('Accuracy = ' + str(score_partitionBinaire))
+print('----')
+print('\n')
+print('-----------------------------------------------------')
--- partitionBinaire.py
+++ partitionBinaire.py
@@ -2,54 +2,33 @@
 # Regroupement hiérarchique (Paritionnement binaire)
 from sklearn.cluster import AgglomerativeClustering
 import numpy as np
-from mnist_similarity import mnist_import_Xtrain_Ytrain_matDiss
+#from mnist_similarity import mnist_import_Xtrain_Ytrain_matDiss
 #import scipy.cluster._hierarchy as shc
 import matplotlib.pyplot as plt
 
-print('###------ Algorithme Partition Binaire ------####')
-print("---importing data.....")
-x_train, y_train, x_test, y_test, D_train, D_test = mnist_import_Xtrain_Ytrain_matDiss()
-print('# Data train = ' + str(len(x_train)))
-print('# Data test = ' + str(len(x_test)))
-print(np.shape(D_train))
-print(np.shape(D_test))
-print("- imported ")
+# print('###------ Algorithme Partition Binaire ------####')
+#print("---importing data.....")
+#x_train, y_train, x_test, y_test, D_train, D_test = mnist_import_Xtrain_Ytrain_matDiss()
+# print('# Data train = ' + str(len(x_train)))
+# print('# Data test = ' + str(len(x_test)))
+# print(np.shape(D_train))
+# print(np.shape(D_test))
+#print("- imported ")
 
-print('... Processing ....')
-
-
-def agglomerative_clustering_predict(agglomerative_clustering, dissimilarity_matrix):
-    average_dissimilarity = list()
-    for i in range(agglomerative_clustering.n_clusters):
-        ith_clusters_dissimilarity = dissimilarity_matrix[:, np.where(
-            agglomerative_clustering.labels_ == i)[0]]
-        average_dissimilarity.append(ith_clusters_dissimilarity.mean(axis=1))
-    return np.argmin(np.stack(average_dissimilarity), axis=0)
+#print('... Processing ....')
 
 
-agglomerative_clustering = AgglomerativeClustering(
-    n_clusters=30, affinity='precomputed', linkage='average')
-agglomerative_clustering.fit(D_train)
-
-# pour train
-agglo_mnist = agglomerative_clustering_predict(
-    agglomerative_clustering, D_train)
-# print(agglo_mnist)  # --> besoin d'association a classe
-print('size array agglo_mnist = ' + str(len(agglo_mnist)))
-
-# pour test
-agglo_mnist_test = agglomerative_clustering_predict(
-    agglomerative_clustering, D_test)
-print('size array agglo_mnist_test = ' + str(len(agglo_mnist_test)))
-# print(agglo_mnist_test)
-# appel une autre fois pour dissim de test et determine class
-# avec l'association de train --> compare avec y_test
-
-print(" - done")
-# pour determiner les class de partition,array_partition sous forme np.array
+def accuracy(y_predict, y_trueValue):
+    a = np.array(y_predict)
+    b = np.array(y_trueValue)
+    # print(y_predict)
+    # print(y_test)
+    succes = np.sum(a == b)
+    print(succes)
+    return round(succes/len(y_predict), 5)
 
 
-def class_partition(array_partition):
+def class_partition(y_train, array_partition):
     class_partition = []
     for i in range(30):
         searchval = i  # partition i
@@ -67,12 +46,6 @@ def class_partition(array_partition):
     return class_partition
 
 
-classification_partition = class_partition(agglo_mnist)
-print(classification_partition)
-
-print(" - Prediction and accuracy : ")
-
-
 def y_prediction(array_partition, classification):
     y_predict = []  # array contient classification selon partition
     for i in range(len(array_partition)):
@@ -86,19 +59,49 @@ def y_prediction(array_partition, classification):
     return y_predict
 
 
-def accuracy(y_predict, y_trueValue):
-    a = np.array(y_predict)
-    b = np.array(y_trueValue)
+def agglomerative_clustering_predict(agglomerative_clustering, dissimilarity_matrix):
+    average_dissimilarity = list()
+    for i in range(agglomerative_clustering.n_clusters):
+        ith_clusters_dissimilarity = dissimilarity_matrix[:, np.where(
+            agglomerative_clustering.labels_ == i)[0]]
+        average_dissimilarity.append(ith_clusters_dissimilarity.mean(axis=1))
+    return np.argmin(np.stack(average_dissimilarity), axis=0)
+
+
+def PB_processing(D_train, D_test, y_train, clusters):
+    agglomerative_clustering = AgglomerativeClustering(
+        n_clusters=clusters, affinity='precomputed', linkage='average')
+    agglomerative_clustering.fit(D_train)
+
+    # pour train
+    agglo_mnist = agglomerative_clustering_predict(
+        agglomerative_clustering, D_train)
+
+    # print(agglo_mnist)  # --> besoin d'association a classe
+    #print('size array agglo_mnist = ' + str(len(agglo_mnist)))
+
+    # pour test
+    agglo_mnist_test = agglomerative_clustering_predict(
+        agglomerative_clustering, D_test)
+
+    #print('size array agglo_mnist_test = ' + str(len(agglo_mnist_test)))
+    # print(agglo_mnist_test)
+    # appel une autre fois pour dissim de test et determine class
+    # avec l'association de train --> compare avec y_test
+
+    #print(" - done")
+    # pour determiner les class de partition,array_partition sous forme np.array
+
+    classification_partition = class_partition(y_train, agglo_mnist)
+    print('Class partition : ' + str(classification_partition))
+    print(classification_partition)
+
+    #print(" - Prediction and accuracy : ")
+
+    y_predict = y_prediction(agglo_mnist_test, classification_partition)
+    return y_predict
     # print(y_predict)
-    # print(y_test)
-    succes = np.sum(a == b)
-    print(succes)
-    return round(succes/len(y_predict), 5)
 
-
-y_predict = y_prediction(agglo_mnist_test, classification_partition)
-print(y_predict)
-
-score = accuracy(y_predict, y_test)
-print('Accuracy = ' + str(score))
-print('----')
+    #score = accuracy(y_predict, y_test)
+    # print('Accuracy = ' + str(score))
+    # print('----')
